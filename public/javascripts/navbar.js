82db7a19c986188e60dc6dd5af1bb684fe794823
Anete Stare <anete.stare@gmail.com>
2022-11-24 11:24:06 +0200

    delete navbar.js file

--- public/javascripts/navbar.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    document.scroll(function(){
-        const nav = document.querySelector('#mainNavbar');
-        nav.toggleClass('scrolled', this.scrollTop()>nav.height());
-
-    })
-})
-
-
