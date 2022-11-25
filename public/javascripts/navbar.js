a9722b21704003b08dc06f0fba4b695ba92f3dce
Anete Stare <anete.stare@gmail.com>
2022-11-25 15:39:00 +0200

    change navbar text color when scroll

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,12 +1,18 @@
 
-
 const navbar = document.querySelector('#mainNavbar')
+const links =document.querySelectorAll('.nav-link')
+
         window.onscroll = function () {
             // pageYOffset or scrollY
             if (window.scrollY > navbar.scrollHeight) {
                 navbar.classList.add('scrolled')
+                links.forEach(v=> {
+                    v.classList.add('scrolled')
+                })
             } else {
                 navbar.classList.remove('scrolled')
+                links.forEach(v=>{
+                    v.classList.remove('scrolled')
+                })
             }
         }
-
